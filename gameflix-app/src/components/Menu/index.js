bf062fd5fe91daf3f00eb3abd643c0071aca7468
Lycia <lyciakremer@gmail.com>
2020-07-29 00:30:03 -0300

    Lycia Kremer - Upgrade do front, adição de conteúdo parcial na tela principal, cofigurações do projeto

--- gameflix-app/src/components/Menu/index.js
+++ gameflix-app/src/components/Menu/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
+import ButtonLink from '../Button';
+//import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return(
-    <nav>
-        <img class="Logo" src={Logo} alt="GameFlix logo"/>
+    <nav className="Menu">
+        <a href="/">
+            <img className="Logo" src={Logo} alt="GameFlix logo"/>
+        </a>
+
+        <ButtonLink className="ButtonLink" href="/">
+            Novo vídeo
+        </ButtonLink>
     </nav>
     );
 }
